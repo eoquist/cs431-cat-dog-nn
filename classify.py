eb7a49dbfead64af3857c8d7899488d6f706a3a0
Emilee Oquist <emiwemi495@gmail.com>
2023-04-24 08:56:37 -0700

    training sadge

--- classify.py
+++ classify.py
@@ -9,12 +9,34 @@ __author__ = "Emilee Oquist"
 __license__ = "MIT"
 __date__ = "April 2023"
 
-import os
 import sys
+import cv2
+from keras.models import load_model
+from keras.preprocessing import image
+import numpy as np
 
 if __name__ == "__main__":
     if(len(sys.argv) < 3):
         print('Usage: python classify.py <neural network> <image file to classify>\n', file=sys.stderr)
         exit(1)
-    for i, arg in enumerate(sys.argv):
-        print(f"Argument {i:>6}: {arg}")
+        # for i, arg in enumerate(sys.argv):
+        #     print(f"Argument {i:>6}: {arg}")
+
+    # Load the trained model
+    model = load_model(sys.argv[1])
+
+    for iterator, filename in enumerate(sys.argv):
+        if iterator >= 2:
+            # Load and preprocess the image
+            # img = image.load_img(filename, target_size=(100, 100))
+            image = cv2.imread(filename)
+
+            if image is not None:
+                if len(image.shape) == 2: # convert greyscale images to color
+                    image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
+                image = image.astype('uint8') / 255.0
+
+            # Classify the image
+            prediction = model.predict(image)
+            class_idx = np.argmax(prediction)
+            print('Predicted:', class_idx)
--- make_nn.py
+++ make_nn.py
@@ -12,7 +12,6 @@ __author__ = "Emilee Oquist"
 __license__ = "MIT"
 __date__ = "April 2023"
 # Credits to: Patrick Loeber and his CNN tutorial video
-import time
 
 import os
 import sys
@@ -24,15 +23,14 @@ import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
 from keras import layers, Sequential, Model
+from keras.models import load_model
 from keras.preprocessing.image import ImageDataGenerator
+
 # from sklearn.model_selection import train_test_split
 
 # globals
 image_directory = None
 nn_model_filename = 'emilee.dnn'
-
-test_images = None
-test_labels = None
 
 def create_training():
     """ Handling the give directory containing training images. """
@@ -41,28 +39,25 @@ def create_training():
     train_images = np.zeros((num_files, 100, 100, 3), dtype=np.uint8)
     train_labels = np.zeros(len(file_names))
 
-    start = time.time() # !!!
     for iterator, filename in enumerate(file_names):
         image = cv2.imread(os.path.join(image_directory, filename))
         if image is not None:
             if len(image.shape) == 2: # convert greyscale images to color
                 image = cv2.cvtColor(image, cv2.COLOR_GRAY2RGB)
-            # img = img.astype('uint8') / 255.0
+            image = image.astype('uint8') / 255.0
             train_images[iterator] = image
             train_labels[iterator] = 1 if filename.startswith('c') else 0
-    end = time.time() # !!!
-    print("create training set time")
-    print(end - start) # !!!
 
     # test set
     permutation = np.random.permutation(num_files)
     shuffled_images = train_images[permutation]
     shuffled_labels = train_labels[permutation]
     # Split the data into training and testing sets
-    train_images = shuffled_images[:800]
-    train_labels = shuffled_labels[:800]
-    test_images = shuffled_images[800:]
-    test_labels = shuffled_labels[800:]
+    split_idx = int(len(shuffled_images) * 0.8)
+    train_images = shuffled_images[:split_idx]
+    train_labels = shuffled_labels[:split_idx]
+    test_images = shuffled_images[split_idx:]
+    test_labels = shuffled_labels[split_idx:]
 
     return train_images, train_labels, test_images, test_labels
 
@@ -80,53 +75,75 @@ def build_model():
         # Convolutional Layers
         # 32 filters, 3x3 kernel
         layers.Conv2D(32, (3, 3), strides=(2,2), padding="same", activation='relu', input_shape=(100, 100, 3)),
+        # layers.BatchNormalization(),
         layers.MaxPooling2D((2, 2)),
-        layers.Dropout(0.5),
+        # layers.Dropout(0.5),
         layers.Conv2D(64, (3, 3), activation='relu'),
+        layers.BatchNormalization(),
         layers.MaxPooling2D((2, 2)),
-        layers.Dropout(0.5),
+        # layers.Dropout(0.5),
         # Dense Layers
         layers.Flatten(),
         layers.Dense(128, activation='relu'),
-        # layers.Dropout(0.5),
-        # layers.Dense(1, activation='sigmoid')
+        layers.Dropout(0.5),
+        layers.Dense(32, activation='relu'),
+        layers.Dropout(0.5),
+        layers.Dense(1, activation='sigmoid')
         # “softmax” layer that is trained on [1,0] for cats and [0,1] for dogs (or vice-versa). 
         # dense sigmoid allegedly better for binary classification over the general softmax
         # layers.Softmax()
     ])
-    # print(model.summary())
+    print(model.summary())
 
     # loss and optimizer
-    model.compile(optimizer='adam',
-              loss='binary_crossentropy', # only cat vs. dog
-              metrics=['accuracy'])
+    model.compile(optimizer='adam',loss='binary_crossentropy', metrics=['accuracy'])
 
     # training
-    batch_size = 64
-    epochs = 5
-
-    model.fit(train_images, train_labels, epochs=epochs,
-            batch_size=batch_size, verbose=2)
+    batch_size = 32
+    epochs = 30
+    model.fit(train_images, train_labels, epochs=epochs, batch_size=batch_size, verbose=1)
 
     # evaulate
-    model.evaluate(test_images,  test_labels, batch_size=batch_size, verbose=2)
-
-
-    model.save(nn_model_filename, include_optimizer = False)
-    # This means the model can't be trained anymore, but it will lead to a more manageable file size.
+    model.evaluate(test_images,  test_labels, batch_size=batch_size, verbose=1)
+    # model.save(nn_model_filename, include_optimizer = False)
+    model.save(nn_model_filename) # allow it to continue training?
     return model
+
+
+def load(model_name):
+    #  Load the training and testing set
+    train_images, train_labels, test_images, test_labels = create_training()
+
+    # model
+    model = load_model(model_name)
+    print("model")
+    print(model)
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    print(model.summary())
+    
+    # Train the model for additional epochs
+    batch_size = 32
+    epochs = 30
+    model.fit(train_images, train_labels, epochs=epochs, batch_size=batch_size, verbose=1)
+    model.evaluate(test_images,  test_labels, batch_size=batch_size, verbose=1)
+    model.save(model_name) # allow it to continue training?
+
 
 
 if __name__ == "__main__":
     """ Handles command line arguments: an image directory and string name of the neural network file to create."""
-    if(len(sys.argv) < 3):
+    if(len(sys.argv) == 2):
+        image_directory = "/Users/ecmo/cs431-cat-dog-nn/cats-and-dogs"
+        nn_model_filename = sys.argv[1]
+        load(nn_model_filename) # python make_nn.py continue_training.h5
+    elif(len(sys.argv) < 3):
         print('Usage: python make_nn.py <image directory> <name of neural network file to create> \n', file=sys.stderr)
         exit(1)
-    
-    # python make_nn.py /Users/ecmo/cs431-cat-dog-nn/cats-and-dogs eoquist
-    image_directory = sys.argv[1] 
-    nn_model_filename = sys.argv[2]
-    nn_model_filename += ".h5"
+    else:
+        # python make_nn.py /Users/ecmo/cs431-cat-dog-nn/cats-and-dogs 32conv2d64
+        image_directory = sys.argv[1] 
+        nn_model_filename = sys.argv[2]
+        nn_model_filename += ".h5"
 
-    build_model()
+        build_model()
     
